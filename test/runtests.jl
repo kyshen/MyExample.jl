242e6206ec71778141db63af5609448799bde00c
kyshen <kyshen2022@gmail.com>
2024-06-15 12:04:38 +0800

    fix tests. You caught me

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,2 @@
-using MyExample
-using Test
-
-@testset "MyExample.jl" begin
-    # Write your tests here.
-    @test my_f(2,1) == 7
-    @test my_f(2,3) == 13
-    @test my_f(2,3) == 11
-end
+using SafeTestsets
+@safetestset "My f rests" begin include("my_f_tests.jl") end
